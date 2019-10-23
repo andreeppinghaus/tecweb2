6d6682c028ebee3b82d774dd005ed6f3f803a045
Andre Eppinghaus <andreeppinghaus@gmail.com>
2019-10-22 21:57:59 -0300

    aula de orientacao a objetos

---
+++ 20192/objeto/Banco.php
@@ -0,0 +1,11 @@
+<?php
+
+class Banco {
+    private $conn;
+    public function __construct(){
+
+    }
+    public function consultar(){
+
+    }
+}
---
+++ 20192/objeto/Login.php
@@ -0,0 +1,30 @@
+<?php
+require_once ("Banco.php");
+/**
+ * Classe para verificar o login 
+ * e senha
+ */
+class Login {
+    /**
+     * nome do usuario
+     */
+    private $nome;
+    /**
+     * senha do usuário
+     */
+    private $senha;
+    
+    private $banco;
+    public function __construct($banco){
+        $this->banco = $banco;
+    }
+    public function setNome($nome) {
+        $this->nome =$nome;
+    }
+    public function setSenha($senha) {
+        $this->senha =$senha;
+    }
+    public function verifica() {
+        $this->banco->consultar();
+    }
+}
